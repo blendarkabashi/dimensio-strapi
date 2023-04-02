ef84eca7bb64dc696fbde4f8803e02395170ffda
Blendar Kabashi <blendar.h.kabashi@gmail.com>
2023-04-03 01:34:49 +0200

    login work

--- dimensio/components/buttonSlider/index.js
+++ dimensio/components/buttonSlider/index.js
@@ -1,8 +1,9 @@
 const ButtonSlider = ({ buttons, activeButton, setActiveButton }) => {
   return (
     <div className="flex mb-10 border border-blue rounded-md">
-      {buttons.map((button) => (
+      {buttons.map((button, index) => (
         <a
+          key={index}
           onClick={() => setActiveButton(button.id)}
           className={`cursor-pointer w-1/2 text-center border-r p-2 ${
             activeButton == button.id ? 'text-white bg-blue' : ''
--- dimensio/pages/login/index.js
+++ dimensio/pages/login/index.js
@@ -5,8 +5,9 @@ import Image from 'next/image';
 import Input from 'components/input';
 import Button from 'components/button';
 import { useState } from 'react';
+import { useFormik } from 'formik';
 import ButtonSlider from 'components/buttonSlider';
-
+import Axios from 'axiosInstance/instance';
 const index = () => {
   const [activeButton, setActiveButton] = useState(1);
 
@@ -18,6 +19,20 @@ const index = () => {
     { id: 2, name: 'Job Seeker' },
   ];
 
+  const formik = useFormik({
+    initialValues: {
+      type: activeButton,
+      username: '',
+      password: '',
+    },
+    onSubmit: (values) => {
+      const { username, password } = values;
+      Axios.post('auth/local', {
+        identifier: username,
+        password: password,
+      }).then((response) => console.log(response.data));
+    },
+  });
   return (
     <div className="my-[80px]">
       <div className="container">
@@ -26,25 +41,44 @@ const index = () => {
         </div>
         <div className="flex items-center justify-between">
           <div className="max-w-[400px]">
-            <div className="text-5xl font-medium mb-3 text-center">
-              Welcome back!
-            </div>
-            <div className="text-darkGray text-xl mb-[50px] text-center">
-              Please enter your details to log in.
-            </div>
-            <ButtonSlider
-              buttons={buttons}
-              activeButton={activeButton}
-              setActiveButton={setActiveButton}
-            ></ButtonSlider>
-            <Input type="text" label="Email" className="mb-8" />
-            <Input type="password" label="Password" className="mb-3" />
-            <div className="text-right mb-8">
-              <a href="#">Forgot Password?</a>
-            </div>
-            <Button type="submit" className="w-full">
-              Log In
-            </Button>
+            <form onSubmit={formik.handleSubmit}>
+              <div className="text-5xl font-medium mb-3 text-center">
+                Welcome back!
+              </div>
+              <div className="text-darkGray text-xl mb-[50px] text-center">
+                Please enter your details to log in.
+              </div>
+              <ButtonSlider
+                buttons={buttons}
+                activeButton={activeButton}
+                setActiveButton={(value) => {
+                  setActiveButton(value);
+                  formik.values.type = value;
+                }}
+              ></ButtonSlider>
+              <Input
+                id="username"
+                type="text"
+                onChange={formik.handleChange}
+                value={formik.values.username}
+                label="Email"
+                className="mb-8"
+              />
+              <Input
+                id="password"
+                type="password"
+                onChange={formik.handleChange}
+                value={formik.values.password}
+                label="Password"
+                className="mb-3"
+              />
+              <div className="text-right mb-8">
+                <a href="#">Forgot Password?</a>
+              </div>
+              <Button type="submit" className="w-full">
+                Log In
+              </Button>
+            </form>
           </div>
           <div className=" h-[550px] w-[400px]">
             <Image
